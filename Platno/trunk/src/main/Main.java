085242aec4c765690cd387e84904a6f8d266fbbe
v.ovcacik <v.ovcacik@a26dceb1-1a4c-0410-80f0-035bb53ab336>
2008-06-09 07:27:47 +0000

    Initial import.

---
+++ Platno/trunk/src/main/Main.java
@@ -0,0 +1,16 @@
+package main;
+
+import grafika.Window;
+
+public class Main {
+
+	/**
+	 * Spouští aplikaci Plátno.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Window hlavniOkno = new Window();
+		hlavniOkno.init();
+	}
+
+}
