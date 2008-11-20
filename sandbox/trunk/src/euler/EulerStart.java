4421782f895603a622ba860f4198909576a4410a
v.ovcacik <v.ovcacik@a26dceb1-1a4c-0410-80f0-035bb53ab336>
2008-11-20 18:56:34 +0000

    + nevyřešený euler."balancedNumbers"

--- sandbox/trunk/src/euler/EulerStart.java
+++ sandbox/trunk/src/euler/EulerStart.java
@@ -6,7 +6,28 @@ public class EulerStart {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-
+		System.out.println(T(1d));
 	}
 
+	private static double T(double n) {
+		double sum = 0;
+		for (double i = Math.pow(10, n) - 1; i > 0; i--) {
+			if (balanced(i)) {
+				sum += i;
+			}
+		}
+		return sum;
+	}
+
+	private static boolean balanced(double i) {
+		String bali = String.valueOf((int) i);
+		double firstsum = 0;
+		double secondsum = 0;
+		for (int j = 0; j <= (bali.length() / 2); j++) {
+			firstsum += Integer.parseInt(bali.substring(j, j + 1));
+			secondsum += Integer.parseInt(bali.substring(bali.length() - 1 - j, bali.length() - j));
+		}
+		if (firstsum == secondsum) return true;
+		return false;
+	}
 }
