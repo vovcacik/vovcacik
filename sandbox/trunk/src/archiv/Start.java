c5079927d1c9f45eaab42fef5ffa16df68a1e723
v.ovcacik <v.ovcacik@a26dceb1-1a4c-0410-80f0-035bb53ab336>
2008-11-22 15:32:46 +0000

    archiv s vrstvama

---
+++ sandbox/trunk/src/archiv/Start.java
@@ -0,0 +1,61 @@
+package archiv;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class Start {
+	public static void main(String a[]) {
+		try {
+			File inFolder = new File("C:/test/zabal/kufr2/");
+			File outFolder = new File("C:/test/zabal/out.zip");
+			ZipOutputStream out = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(outFolder)));
+			BufferedInputStream in = null;
+			byte[] data = new byte[1000];
+			File files[] = seznam(inFolder.listFiles());
+			for (File f : files) {
+				System.out.println(f.getPath());
+			}
+			for (int i = 0; i < files.length; i++) {
+				if (files[i].isFile()) {
+					in = new BufferedInputStream(new FileInputStream(files[i].getPath()), 1000);
+				}
+				out.putNextEntry(new ZipEntry(files[i].getPath()));
+				if (files[i].isFile()) {
+					int count;
+					while ((count = in.read(data, 0, 1000)) != -1) {
+						out.write(data, 0, count);
+					}
+				}
+				out.closeEntry();
+			}
+			out.flush();
+			out.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static File[] seznam(File[] list) {
+		List<File> seznam = new ArrayList<File>();
+		for (File f : list) {
+			seznam.add(f);
+		}
+		for (int i = 0; i < list.length; i++) {
+			if (list[i].isDirectory()) {
+				for (File f2 : seznam(list[i].listFiles())) {
+					seznam.add(f2);
+				}
+			}
+		}
+		File[] ret = new File[seznam.size()];
+		seznam.toArray(ret);
+		return ret;
+	}
+}
