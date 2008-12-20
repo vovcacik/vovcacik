0385ebbc43d72d433df18cc92e742b1ab48eea86
v.ovcacik <v.ovcacik@a26dceb1-1a4c-0410-80f0-035bb53ab336>
2008-12-20 18:37:52 +0000

    + JUnit 4 nový sandbox

---
+++ sandbox/trunk/src/junit4/Start.java
@@ -0,0 +1,16 @@
+package junit4;
+
+public class Start {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+	}
+
+	private int add(int a, int b) {
+		return a + b;
+	}
+
+}
---
+++ sandbox/trunk/src/junit4/TestCase.java
@@ -0,0 +1,42 @@
+package junit4;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TestCase {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		System.out.println("BeforeClass");
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		System.out.println("AfterClass");
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		System.out.println("Before");
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		System.out.println("After");
+	}
+
+	@Test
+	public void add() {
+		assertTrue(true);
+	}
+	
+	@Test 
+	public void add2() {
+		assertFalse(false);
+	}
+}
